48e5acfe7c0b7a0dcc17f9626c2cc53e6f85d763
CHARLES Clément <Clement.charlesC@gmail.com>
2021-08-14 12:55:47 +0200

    Form & Field establshed, background image done

--- src/Pages/Login/Login.view.jsx
+++ src/Pages/Login/Login.view.jsx
@@ -1,11 +1,38 @@
+import { Form, Field } from 'react-final-form';
+import RenderTextInput from '../../Renderers/RenderTextInput';
 import { loginImagePath, legalText } from '../../config';
+// Using 'loginImagePath' directly in 'login.module.scss' with the 'background-image'  react attribute which is meant for that purpose
 import { palette } from '../../muiTheme';
-
 import styles from './login.module.scss';
 
 function LoginView({ onSubmit, errorMsg }) {
   return (
     <div className={styles.loginWrapper}>
+      <Form
+        onSubmit={onSubmit}
+        render={({ handleSubmit }) => (
+          <form onSubmit={handleSubmit} className={styles.form}>
+            <Field
+              placeholder="Username"
+              name="Username"
+              type="password"
+              component={RenderTextInput}
+            />
+            <Field
+              placeholder="Password"
+              name="Password"
+              type="password"
+              component={RenderTextInput}
+            />
+            <a href="/forgotPassword" className={styles.link}>
+              Forgot password ?
+            </a>
+            <button type="submit" className={styles.btn}>
+              LOGIN
+            </button>
+          </form>
+        )}
+      />
       <div className={styles.copyrightText}>{legalText}</div>
     </div>
   );
